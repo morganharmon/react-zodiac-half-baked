b46eacce2629b7e90204103e04cb37370fac192f
Morgan Harmon <morganharmon@gmail.com>
2022-06-06 15:04:30 -0700

    Everything but images

--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -1,5 +1,13 @@
 import './ZodiacCard.css';
+import aquarius from '../images/aquarius.png';
 
-export default function ZodiacCard() {
+export default function ZodiacCard({ name, dates }) {
   {/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
+  return (
+    <div>
+      <p>{ name }</p>
+      <p>{ dates }</p>
+      <img src={aquarius} />
+    </div>
+  );
 }
